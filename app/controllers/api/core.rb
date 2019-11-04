7630ea9a8d0739a7986aefed5c153f276778222c
Łukasz Szmelc <lukasz.szmelc@netguru.pl>
2019-11-04 21:46:38 +0100

    Add authorization-related swagger settings

--- app/controllers/api/core.rb
+++ app/controllers/api/core.rb
@@ -9,8 +9,11 @@ module API
       mount_path: '/docs',
       info: {
         title: 'Passion.io Recruitment Task',
-        contact_name: 'Lukasz',
+        contact_name: 'Lukasz Szmelc',
         contact_email: 'lszmelc@gmail.com'
+      },
+      security_definitions: {
+        api_key: { type: 'oauth', name: 'Authorization', in: 'header' }
       }
   end
 end
--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -7,3 +7,7 @@ GrapeSwaggerRails.options.url = '/api/docs'
 GrapeSwaggerRails.options.app_name = 'Passion.io Recruitment Task'
 GrapeSwaggerRails.options.hide_url_input = true
 GrapeSwaggerRails.options.hide_api_key_input = false
+
+GrapeSwaggerRails.options.api_auth = 'bearer'
+GrapeSwaggerRails.options.api_key_name = 'Authorization'
+GrapeSwaggerRails.options.api_key_type = 'header'
