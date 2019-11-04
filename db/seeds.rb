50ce7551cf2f6207dbdc76180d28647718443c73
Łukasz Szmelc <lukasz.szmelc@netguru.pl>
2019-11-04 20:43:15 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+display_names = [
+  'Anowa',
+  'Brittnebabe',
+  'Barstarzz',
+  'Russell Brunson',
+  'Tim Nilson',
+  'J. K. Rowling',
+  'Stephen Hawking',
+  'Linkin Park',
+  'Lang Lang',
+  'Music Performance for Beginners',
+  'Learn the Piano',
+  'Jimmy Page'
+]
+
+puts '---- Generating Users ----'
+display_names.each do |name|
+  User.create!(
+    email: Faker::Internet.email,
+    display_name: name,
+    password: '123123',
+    password_confirmation: '123123'
+  )
+end
+
+verticals = JSON.parse(File.read(Rails.root.join('db', 'seeds_files', 'verticals.json')))
+categories = JSON.parse(File.read(Rails.root.join('db', 'seeds_files', 'categories.json')))
+courses = JSON.parse(File.read(Rails.root.join('db', 'seeds_files', 'courses.json')))
+
+puts '---- Generating Verticals ----'
+verticals.each do |vertical|
+  Vertical.create!(name: vertical['Name'])
+end
+
+puts '---- Generating Categories ----'
+categories.each do |category|
+  Category.create!(name: category['Name'], vertical: Vertical.find(category['Verticals']), state: category['State'])
+end
+
+puts '---- Generating Courses ----'
+courses.each do |course|
+  Course.create!(name: course['Name'], author: User.find_by(display_name: course['Author']), category_id: course['Categories'], state: course['State'])
+end
+
+puts '---- Seeds completed ----'
